7f909f4915e60189ad2d25dedd682cd67ff63b6c
Luiz Felipe <Luiz Felipe@Luiz-PC>
2018-10-06 16:22:34 -0300

    Inclusão novas classes.

--- Colossus 1.1/src/felipe/colossus/cadastro/Produto.java
+++ Colossus 1.1/src/felipe/colossus/cadastro/Produto.java
@@ -1,5 +1,21 @@
 package felipe.colossus.cadastro;
 
 public class Produto {
-
+	private long id;
+	private long codigo_barras;
+	private String descricao;
+	private Fabricante fabricante;
+	private Grupo grupo;
+	private Subgrupo subgrupo;
+	private Setor setor;
+	private long ncm;
+	private StatusProduto statusproduto;
+	private boolean enviaBalanca;
+	private boolean controlaEstoque;
+	private boolean composicaoProducao;
+	private boolean composicaoVendaDireta;
+	private boolean vendaFracionada;
+	private boolean permiteDesconto;
+	private String detalhesProduto;
+	private int informacoesAdicionaisNFE;
 }
